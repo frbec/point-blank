9aa8690417e506c32c307f83a164ad965223b6a5
frbec <fredrikbeckius@gmail.com>
2017-10-29 09:49:40 +0100

    Smaller transparent nav header and nuxt-links.

--- client/components/header-nav.vue
+++ client/components/header-nav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav role="navigation">
-    <a href="/#intro">About<img src="../assets/img/ic_about-me.svg"></a>
-    <a href="/#portfolio">Portfolio<img src="../assets/img/ic_portfolio.svg"></a>
+    <nuxt-link to="/#intro">About<img src="../assets/img/ic_about-me.svg"></nuxt-link>
+    <nuxt-link to="/#portfolio">Portfolio<img src="../assets/img/ic_portfolio.svg"></nuxt-link>
   </nav>
 </template>
