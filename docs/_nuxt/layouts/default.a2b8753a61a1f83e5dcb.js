6fe682658fdc1974ea5780861d66c672addb730c
frbec <fredrikbeckius@gmail.com>
2017-11-02 15:03:09 +0100

    Generate new site, moved to docs for github pages.

---
+++ docs/_nuxt/layouts/default.a2b8753a61a1f83e5dcb.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{Ma2J:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=t("aqwf"),u=t("VU/8"),a=u(null,r.a,!1,null,null,null);n.default=a.exports},aqwf:function(e,n,t){"use strict";var r=function(){var e=this,n=e.$createElement;return(e._self._c||n)("nuxt")},u=[],a={render:r,staticRenderFns:u};n.a=a}});
+//# sourceMappingURL=default.a2b8753a61a1f83e5dcb.js.map
